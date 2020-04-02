03889654a3b66ea59b649b92b0fb7e51f8695b14
Su Anli <sual0001@gmail.com>
2020-04-02 11:04:04 +0800

    feat(app): add firebase, show recipe titles (#3)

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,13 +2,31 @@
  * @format
  */
 
-import 'react-native';
 import React from 'react';
+import 'react-native';
+import {render} from 'react-native-testing-library';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+const mockUnsubscribe = jest.fn();
+jest.mock('@react-native-firebase/firestore', () => ({
+  __esModule: true,
+  default: () => ({
+    collection: () => ({
+      onSnapshot: (callback: any) => {
+        callback({
+          docs: [
+            {id: 'RECIPE_ID_A', data: () => ({title: 'RECIPE_TITLE_A'})},
+            {id: 'RECIPE_ID_B', data: () => ({title: 'RECIPE_TITLE_B'})},
+          ],
+        });
+        return () => mockUnsubscribe;
+      },
+    }),
+  }),
+}));
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('Given data, When I open App, Then I should see recipes title "RECIPE_TITLE_A", "RECIPE_TITLE_B"', async () => {
+  const {getByText} = render(<App />);
+  expect(getByText('RECIPE_TITLE_A')).toBeDefined();
+  expect(getByText('RECIPE_TITLE_B')).toBeDefined();
 });
--- e2e/app.test.ts
+++ e2e/app.test.ts
@@ -1,11 +1,15 @@
-import {by, device, element, expect} from 'detox';
+import {by, device, element, expect, waitFor} from 'detox';
 
 describe('App', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
 
-  it('Given any, When I open App, Then I should see "Welcome to React"', async () => {
-    await expect(element(by.text('Welcome to React'))).toBeVisible();
+  it('Given any, When I open App, Then I should see recipes title "E2E_TITLE_A"', async () => {
+    const title = element(by.text('E2E_TITLE_A'));
+    await waitFor(title)
+      .toHaveText('E2E_TITLE_A')
+      .withTimeout(10000);
+    await expect(title).toBeVisible();
   });
 });
